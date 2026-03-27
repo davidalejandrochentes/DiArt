5baf21c468222ab76cec0f45325ee641941cab7a
david alejandro chentes <davidalejandrochentes@gmail.com>
2026-03-27 12:48:10 -0400

    v.0.5

--- portafoleo/urls.py
+++ portafoleo/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, categoria_detalle
 
 app_name = 'portafoleo'
 
 urlpatterns = [
     path('', index, name='index'),
+    path('categoria/<slug:slug>/', categoria_detalle, name='categoria_detalle'),
 ]
--- portafoleo/views.py
+++ portafoleo/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
-from .models import Portafolio, CategoriaPortafolio
+from django.shortcuts import render, get_object_or_404
+from .models import Portafolio, CategoriaPortafolio, ImagenCategoria
 
 
 def index(request):
     portafolio = Portafolio.objects.filter(activo=True).first()
     categorias = CategoriaPortafolio.objects.filter(activo=True)
-    
+
     context = {
         'portafolio': portafolio,
         'categorias': categorias,
     }
     return render(request, 'portafoleo/index.html', context)
+
+
+def categoria_detalle(request, slug):
+    portafolio = Portafolio.objects.filter(activo=True).first()
+    categoria = get_object_or_404(CategoriaPortafolio, slug=slug, activo=True)
+    imagenes = ImagenCategoria.objects.filter(categoria=categoria, activo=True)
+
+    context = {
+        'portafolio': portafolio,
+        'categoria': categoria,
+        'imagenes': imagenes,
+    }
+    return render(request, 'portafoleo/categoria_detalle.html', context)
